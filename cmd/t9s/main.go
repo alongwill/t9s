5ca1a7b240e1ee48c9d0ae5d39c3de6fce9ea5f8
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2025-03-28 18:04:12 +0300

    feat: add initial example
    Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

--- cmd/t9s/main.go
+++ cmd/t9s/main.go
@@ -2,22 +2,102 @@ package main
 
 import (
 	"context"
+	"github.com/cosi-project/runtime/pkg/safe"
+	"github.com/siderolabs/talos/pkg/machinery/resources/block"
+	"github.com/siderolabs/talos/pkg/machinery/resources/cluster"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
+	"os"
+	"time"
 
 	"github.com/siderolabs/talos/pkg/machinery/client"
 )
 
 func main() {
-	println("hello")
+	if err := run(); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+}
 
-	log.Printf("sdfdsfdsf")
+func run() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	ctx := client.WithNode(context.Background(), "172.20.0.3")
+	// READ TALOSCONFIG
 
-	client, err := client.New(ctx)
-	if err != nil {
-		panic(err)
+	talosconfigPath := os.Getenv("TALOSCONFIG")
+	if talosconfigPath == "" {
+		talosconfigPath = os.ExpandEnv("$HOME/.talos/config")
 	}
 
-	_ = client
+	log.Printf("using talosconfig: %s", talosconfigPath)
+
+	// BUILD CLIENT
+
+	cli, err := client.New(ctx, client.WithConfigFromFile(talosconfigPath))
+	if err != nil {
+		return err
+	}
+
+	// TARGET MULTIPLE NODES AT THE SAME TIME - MULTIPLEXING
+
+	ctx = client.WithNodes(ctx, "172.20.0.3", "172.20.0.4", "172.20.0.5")
+
+	// TALOSCTL VERSION:
+
+	version, err := cli.MachineClient.Version(ctx, &emptypb.Empty{})
+	if err != nil {
+		return err
+	}
+
+	for _, message := range version.GetMessages() {
+		log.Printf("node: %s version: %s", message.Metadata.Hostname, message.Version.String())
+	}
+
+	// TALOSCTL DISKS - THE DEPRECATED WAY
+
+	disksResponse, err := cli.StorageClient.Disks(ctx, &emptypb.Empty{})
+	if err != nil {
+		return err
+	}
+
+	for _, disks := range disksResponse.Messages {
+		log.Printf("node: %s disks:", disks.Metadata.Hostname)
+		for _, disk := range disks.Disks {
+			log.Printf("  disk: %s, size: %d, type: %s", disk.DeviceName, disk.Size, disk.Type.String())
+		}
+	}
+
+	// COSI EXAMPLES - RESOURCES - TALOSCTL GET ...
+
+	cosiState := cli.COSI
+
+	// COSI state API calls do not support multiplexing (.WithNodes), so we target a single node.
+	ctx = client.WithNode(ctx, "172.20.0.3")
+
+	// TALOSCTL GET MEMBERS
+
+	memberList, err := safe.StateListAll[*cluster.Member](ctx, cosiState)
+	if err != nil {
+		return err
+	}
+
+	for member := range memberList.All() {
+		log.Printf("got member %q, type: %q", member.Metadata().ID(), member.TypedSpec().MachineType.String())
+	}
+
+	// TALOSCTL GET DISKS (THE NEW WAY TO READ DISKS)
+
+	diskList, err := safe.StateListAll[*block.Disk](ctx, cosiState)
+	if err != nil {
+		return err
+	}
+
+	for disk := range diskList.All() {
+		log.Printf("got disk %q, transport: %s", disk.Metadata().ID(), disk.TypedSpec().Transport)
+	}
+
+	// THE END
+
+	return nil
 }
